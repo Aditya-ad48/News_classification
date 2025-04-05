b78793cb6823ecdab559d5d31660242f72268d6a
Aditya-ad48 <adityaraj13.ahirwar@gmail.com>
2025-04-05 22:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from nltk.corpus import stopwords
 import string
 
 
-nltk.download('punkt')
-nltk.download('stopwords')
+import os
+
+
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.data.path.append(nltk_data_path)
+
 
 @st.cache_resource
 def load_model():
